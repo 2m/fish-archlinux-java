2f20bf4737a4c919b4e3e7eda7267d9c77cd678f
Martynas Mickevičius <self@2m.lt>
2019-05-30 09:05:39 +0300

    Fix command

--- completions/archlinux-java.fish
+++ completions/archlinux-java.fish
@@ -6,8 +6,8 @@ function __fish_aj_needs_command
   return 1
 end
 
-complete -f -c jabba -n '__fish_aj_needs_command' -a status -d "List installed Java environments and enabled one"
-complete -f -c jabba -n '__fish_aj_needs_command' -a get -d "Return the short name of the Java environment set as default"
-complete -f -c jabba -n '__fish_aj_needs_command' -a set -d "Force <JAVA_ENV> as default"
-complete -f -c jabba -n '__fish_aj_needs_command' -a unset -d "Unset current default Java environment"
-complete -f -c jabba -n '__fish_aj_needs_command' -a fix -d "Fix an invalid/broken default Java environment configuration"
+complete -f -c archlinux-java -n '__fish_aj_needs_command' -a status -d "List installed Java environments and enabled one"
+complete -f -c archlinux-java -n '__fish_aj_needs_command' -a get -d "Return the short name of the Java environment set as default"
+complete -f -c archlinux-java -n '__fish_aj_needs_command' -a set -d "Force <JAVA_ENV> as default"
+complete -f -c archlinux-java -n '__fish_aj_needs_command' -a unset -d "Unset current default Java environment"
+complete -f -c archlinux-java -n '__fish_aj_needs_command' -a fix -d "Fix an invalid/broken default Java environment configuration"
